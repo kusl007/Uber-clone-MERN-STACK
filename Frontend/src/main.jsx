d0c8a4c66343fd825d9677365831ae4c9ead9d8a
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-11-28 22:27:28 +0545

    frontend register and signin done

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import UserContext from './context/UserContext.jsx'
+import CaptainContext from './context/CapatainContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
+    <UserContext>
+      <CaptainContext>
+
+    <BrowserRouter>
     <App />
-  </StrictMode>,
+    </BrowserRouter>
+    </CaptainContext>
+    </UserContext>
+  // </StrictMode>,
 )
